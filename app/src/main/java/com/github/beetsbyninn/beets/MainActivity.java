57d4ac6075acdfde488f54af2c160b2bd2e6dcb0
CodeSmasher2000 <jonatanwahlstedt@gmail.com>
2017-02-23 14:01:54 +0100

    First version of the MusicPlayer
    Can play a song in the background and a sound every time we push a
    button.

--- app/src/main/java/com/github/beetsbyninn/beets/MainActivity.java
+++ app/src/main/java/com/github/beetsbyninn/beets/MainActivity.java
@@ -2,12 +2,57 @@ package com.github.beetsbyninn.beets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private MusicPlayer mMusicPlayer;
+    private Button mBtnPlay;
+    private Button mBtnStep;
+    private boolean mIsplaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnPlay = (Button)findViewById(R.id.btnPlay);
+        mBtnPlay.setOnClickListener(new ButtonPlayListener());
+        mMusicPlayer = new MusicPlayer(this);
+        mMusicPlayer.initSongMediaPlayer();
+        try {
+            mMusicPlayer.initStepMediaPlayer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mBtnStep = (Button)findViewById(R.id.btnStep);
+        mBtnStep.setOnClickListener(new ButtonStepListener());
+
+
+    }
+
+    private class ButtonPlayListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(mIsplaying == false) {
+                mMusicPlayer.playSong();
+                mIsplaying = true;
+            }else if(mIsplaying){
+                mMusicPlayer.stopSong();
+                mIsplaying = false;
+            }
+        }
+    }
+
+    private class ButtonStepListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            try {
+                mMusicPlayer.playStep();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/github/beetsbyninn/beets/MusicPlayer.java
@@ -0,0 +1,74 @@
+package com.github.beetsbyninn.beets;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+/**
+ * Handles the Mediaplayer used for playing songs and the sound when taking a step.
+ * Created by Jonatan Wahlstedt and Oscar Sandberg on 23-Feb-17.
+ */
+
+public class MusicPlayer {
+    private MediaPlayer mSongMediaPlayer;
+    private MediaPlayer mStepMediaPlayer;
+    private MainActivity mMainActivity;
+
+    public MusicPlayer(MainActivity mainActivity){
+        mMainActivity = mainActivity;
+    }
+
+    /**
+     * Initializes the StepMediaPlayer and the SongMediaplayer
+     */
+    public void initSongMediaPlayer() {
+        mSongMediaPlayer = MediaPlayer.create(mMainActivity,R.raw.lowrider);
+
+
+    }
+
+    /**
+     * Initializes the StepMediaPlayer and sets the volume on the sound.
+     * @throws IOException
+     */
+    public void initStepMediaPlayer() throws IOException {
+        mStepMediaPlayer = MediaPlayer.create(mMainActivity, R.raw.stepsound);
+        mStepMediaPlayer.setVolume(0.2f,0.2f);
+
+    }
+
+    /**
+     * Starts the SongMediaplayer and plays the initialized song.
+     */
+    public void playSong(){
+        if(!mSongMediaPlayer.isPlaying()){
+            mSongMediaPlayer.start();
+        }
+    }
+
+    /**
+     * Stops the SongMediaplayer by pausing the song.
+     */
+    public void stopSong(){
+        if(mSongMediaPlayer.isPlaying()){
+            mSongMediaPlayer.pause();
+            //mSongMediaPlayer.reset();
+        }
+    }
+
+    /**
+     * Uses Mediaplayer to play a sound then the user takes a step.
+     * @throws IOException
+     */
+    public void playStep() throws IOException {
+        if(!mStepMediaPlayer.isPlaying()){
+            mStepMediaPlayer.start();
+        }else if(mStepMediaPlayer.isPlaying()){
+            mStepMediaPlayer.stop();
+            mStepMediaPlayer.release();
+            initStepMediaPlayer();
+            mStepMediaPlayer.start();
+        }
+    }
+}
